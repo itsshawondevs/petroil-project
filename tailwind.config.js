493adeeb452fb219c80773e36648859b1bb7c608
itsshawondevs <shawonuser24@gmail.com>
2023-12-22 18:45:13 +0600

    service area complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,10 @@ tailwind.config = {
             'poppins': ["'Poppins', sans-serif;"],
         },
         backgroundImage: {
-            'banner': " linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/banner-img.jpg')",
+            'banner': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/banner-img.jpg')",
+            'service_1': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img1.jpg')",
+            'service_2': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img2.jpg')",
+            'service_3': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img3.jpg')",
         },
       }
     }
