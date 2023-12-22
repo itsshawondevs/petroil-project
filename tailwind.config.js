1692f142b5b866b14ce3c4658993afb830a2d116
itsshawondevs <shawonuser24@gmail.com>
2023-12-22 16:43:43 +0600

    banner part complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ tailwind.config = {
         fontFamily: {
             'poppins': ["'Poppins', sans-serif;"],
         },
+        backgroundImage: {
+            'banner': " linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/banner-img.jpg')",
+        },
       }
     }
   }
