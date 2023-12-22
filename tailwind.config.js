32d2405801f332d45ed840912858703bb93626ad
itsshawondevs <shawonuser24@gmail.com>
2023-12-22 15:53:59 +0600

    header part done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        maxWidth: {
+            'container': '1144px',
+        },
+        colors: {
+            'white': '#FFF',
+        },
+        fontFamily: {
+            'poppins': ["'Poppins', sans-serif;"],
+        },
+      }
+    }
+  }
