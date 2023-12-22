c7342c236f08ca792aaf312564d0276f34c34fd5
itsshawondevs <shawonuser24@gmail.com>
2023-12-22 20:44:35 +0600

    about-part done

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ tailwind.config = {
             'service_1': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img1.jpg')",
             'service_2': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img2.jpg')",
             'service_3': "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url('./images/service-img3.jpg')",
+            'about': "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)) ,url('./images/about-img.jpg')"
         },
       }
     }
